f85d99f188f64c8b8ecef0874c76e1bd83c4ef12
darnoc619 <conrad.reyes@eqlink.com>
2022-12-19 23:21:44 +0800

    initial commit

---
+++ assets/custom.js
@@ -0,0 +1,38 @@
+// var $animation_elements = $('.animation-element');
+// var $window = $(window);
+
+// function check_if_in_view() {
+//   var window_height = $window.height();
+//   var window_top_position = $window.scrollTop();
+//   var window_bottom_position = (window_top_position + window_height);
+
+//   $.each($animation_elements, function() {
+//     var $element = $(this);
+//     var element_height = $element.outerHeight();
+//     var element_top_position = $element.offset().top;
+//     var element_bottom_position = (element_top_position + element_height);
+
+
+//     if ((element_bottom_position >= window_top_position) &&
+//       (element_top_position <= window_bottom_position)) {
+//       $element.addClass('in-view');
+//     } else {
+//       $element.removeClass('in-view');
+//     }
+//   });
+// }
+
+// $window.on('scroll resize', check_if_in_view);
+// $window.trigger('scroll');
+
+
+
+// jQuery(window).scroll(function() {    
+//     var scroll = jQuery(window).scrollTop();
+
+//      //>=, not <=
+//     if (scroll >= 1000) {
+//         //clearHeader, not clearheader - caps H
+//         jQuery(".animation-element").toggleClass("in-view");
+//     }
+// });
